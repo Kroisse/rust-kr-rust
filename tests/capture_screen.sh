59021546396dbfabe45738b63106299d724a3aab
Eunchong Yu <kroisse@gmail.com>
2016-04-22 23:32:52 +0900

    Upload screenshot to http://imgur.com/a/qoQhn

--- tests/capture_screen.sh
+++ tests/capture_screen.sh
@@ -5,3 +5,12 @@ SERVER_PID=$!
 trap "kill $SERVER_PID" EXIT
 sleep 5
 phantomjs tests/capture_screen.js
+
+IMAGE_URL=$(
+    curl -X POST -H "Authorization: Client-ID ${IMGUR_CLIENT_ID}" \
+        -F "title=${TRAVIS_BUILD_ID}" \
+        -F "album=${IMGUR_ALBUM_DELETEHASH}" \
+        -F "image=@index.png" \
+        https://api.imgur.com/3/image \
+    | node -pe 'JSON.parse(require("fs").readFileSync("/dev/stdin").toString()).data.link')
+echo "Screenshot upload to ${IMAGE_URL}"
